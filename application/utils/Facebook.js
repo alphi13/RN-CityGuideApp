5c46d9c0a7e0159e4443e3c9090538a57221dc0d
React Native Dev <yunayamda3@gmail.com>
2019-05-02 13:54:37 +0900

    initial commit

---
+++ application/utils/Facebook.js
@@ -0,0 +1,6 @@
+export default {
+	config: {
+		application_id: '532785920254078',
+		permissions: ['public_profile', 'email']
+	}
+};
---
+++ application/utils/Firebase.js
@@ -0,0 +1,10 @@
+
+//////////////////// FIREBASE
+
+export default firebaseConfig = {
+    apiKey: "AIzaSyATUJnO6qLdnqORAMRgE_clWH7Tqkw2MFw",
+    authDomain: "alqassim.x10.ltd",
+    databaseURL: "http://alqassim.x10.ltd",
+    projectId: "alqassim-9c853",
+    storageBucket: "",
+  };
